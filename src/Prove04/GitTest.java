e02e6d38fd55a09ebee00fc606a5259bb798ba49
shadolite <amy.kawaii@gmail.com>
2020-01-31 22:36:44 -0700

    Added response.

--- src/Prove04/GitTest.java
+++ src/Prove04/GitTest.java
@@ -3,5 +3,6 @@ package Prove04;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, my name is John.");
+        System.out.println("Hello John, my name is Amy.");
     }
 }
